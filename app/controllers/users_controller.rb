a1d473eafb1e355a16985096f06599b4eb0fb67f
Moses Ogwo <mosesogwo@gmail.com>
2019-07-25 15:42:42 +0100

    add user edit view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,11 +13,21 @@ class UsersController < ApplicationController
     if @user.save
       redirect_to users_path
     else
-      render 'new'
+      render :new
     end
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to users_path
+    else
+      render :edit
+    end
   end
 
   def show
