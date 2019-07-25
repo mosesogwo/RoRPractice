2cbb43c6c1eca7beddf3b29e8477beb0f3273c43
Moses Ogwo <mosesogwo@gmail.com>
2019-07-25 14:47:39 +0100

    functional new form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,19 @@
 class UsersController < ApplicationController
   def index
   end
-  
+
   def new
+    @user = User.new
   end
 
   def create
+    # @user = User.new(name: [:params][:name], email: [:params][:email], password: [:params][:password])
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -16,4 +24,10 @@ class UsersController < ApplicationController
 
   def destroy
   end
+
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password)
+    end
 end
